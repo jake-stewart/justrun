95f4ac9983b8da1ded76a6f5791a5084b2a6e7e8
Jake Stewart <jake.stephen.stewart@gmail.com>
2024-07-03 17:26:59 +0800

    did shit

---
+++ index.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+
+const { buildSync, formatMessagesSync } = require("esbuild");
+const { spawnSync } = require("child_process");
+const temp = require("temp");
+const fs = require("fs");
+
+function build(scriptPath, outputPath) {
+    const result = buildSync({
+        bundle: true,
+        entryPoints: [scriptPath],
+        format: "cjs",
+        outfile: outputPath,
+    });
+    if (result.warnings.length) {
+        const formatted = formatMessagesSync(result.warnings);
+        console.error("\n".join(formatted));
+    }
+    if (result.errors.length) {
+        const formatted = formatMessagesSync(result.errors);
+        console.error("\n".join(formatted));
+        return false;
+    }
+    return true;
+}
+
+function execute(outputPat, args) {
+    return spawnSync("node", [outputPath, ...args], { stdio: "inherit" }).status;
+}
+
+
+function main() {
+    const args = process.argv.slice(2);
+    const scriptPath = args.shift();
+    const outputPath = temp.path({ suffix: ".js" });
+    let exitStatus = null;
+
+    try {
+        if (!build(scriptPath, outputPath)) {
+            return;
+        }
+        exitStatus = execute(outputPath, args);
+    }
+    finally {
+        fs.rmSync(outputPath);
+    }
+
+    process.exit(exitStatus == null ? -1 : exitStatus);
+}
+
+main();
